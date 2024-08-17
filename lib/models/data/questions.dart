ba42716b1f3eefd1aa55f41f34ff5d3e70755a35
adogbeji <baka_1993@yahoo.co.uk>
2024-08-17 13:50:35 +0100

    Add more dummy data

--- lib/models/data/questions.dart
+++ lib/models/data/questions.dart
@@ -1,4 +1,34 @@
 import 'package:quiz_app_2/models/quiz_question.dart';
 import 'package:quiz_app_2/views/widgets/quiz.dart';
 
-List<QuizQuestion> questions = [];
+List<QuizQuestion> questions = [
+  QuizQuestion(
+    question: 'How many days in a week?',
+    answers: ([4, 9, 2, 7, 5]) as List<String>,
+    correctAnswer: 7 as String
+  ),
+  QuizQuestion(
+    question: 'What fruit is yellow?',
+    answers: [
+      'Pear',
+      'Kiwi',
+      'Apple',
+      'Lemon',
+      'Mango', 
+      'Grapes',
+    ],
+    correctAnswer: 'Lemon',
+  ),
+  QuizQuestion(
+    question: 'Which technology is used for mobile apps?',
+    answers: [
+      'JavaScript',
+      'Node.js',
+      'SQL', 
+      'Docker',
+      'Flutter',
+      'Python'
+    ],
+    correctAnswer: 'Flutter',
+  ),
+];
