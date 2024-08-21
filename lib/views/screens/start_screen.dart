6d88e599e721efe77dbda9f9c70fe151d7fa911a
adogbeji <baka_1993@yahoo.co.uk>
2024-08-21 13:39:06 +0100

    Add assets folder & start building start screen

--- lib/views/screens/start_screen.dart
+++ lib/views/screens/start_screen.dart
@@ -7,8 +7,8 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Start Screen'),
+    return Column(
+      children: [],
     );
   }
 }
