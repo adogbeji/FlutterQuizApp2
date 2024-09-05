230bc20da20bd24ff3bd21f050c3293892976dd3
adogbeji <baka_1993@yahoo.co.uk>
2024-09-05 13:32:48 +0100

    Add code to collect summary data in results screen

--- lib/views/screens/results_screen.dart
+++ lib/views/screens/results_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_2/models/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
@@ -7,6 +8,16 @@ class ResultsScreen extends StatelessWidget {
 
   List<Map<String, Object>> getSummaryData() {
     final List<Map<String, Object>> summary = [];
+
+    for (var x = 0; x < chosenAnswers.length; x++) {
+      summary.add({
+        'question_index': 1,
+        'question': questions[x].question,
+        'chosen_answer': chosenAnswers[x],
+        'correct_answer': questions[x].correctAnswer,
+      });
+    }
+
     return summary;
   }
 
